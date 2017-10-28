f6168da6139cf292fa54370035173a064c12650e
SethEggerRocks <setheggerrocks@gmail.com>
2017-10-27 23:19:24 -0400

    Update excelTranPer.py
    I changed the functions and file so the print statements print a space in between each function call.

--- excelTranPer.py
+++ excelTranPer.py
@@ -1,24 +1,30 @@
 import openpyxl
 
+
 glc_wb = openpyxl.load_workbook('/Users/nichodeturbo/Desktop/Grateful Life Master Roster 170831.xlsx')
 glc_sheet = glc_wb.get_sheet_by_name("Residential")
 glc_otherThing = glc_sheet.max_row
+
 
 do_wb = openpyxl.load_workbook('/Users/nichodeturbo/Desktop/Dougherty House IOP Master Roster August 2017.xlsx')
 do_sheet = do_wb.get_sheet_by_name("OUTPATIENT")
 do_otherThing = do_sheet.max_row
 
+
 dr_wb = openpyxl.load_workbook('/Users/nichodeturbo/Desktop/Master Droege Daily Roster July 2017.xlsx')
 dr_sheet = dr_wb.get_sheet_by_name("Residential")
 dr_otherThing = dr_sheet.max_row
+
 
 de_wb = openpyxl.load_workbook('/Users/nichodeturbo/Desktop/Master Droege Detox Daily Roster july 2017.xlsx')
 de_sheet = de_wb.get_sheet_by_name("DETOX")
 de_otherThing = de_sheet.max_row
 
+
 wb = openpyxl.load_workbook('/Users/nichodeturbo/Desktop/Wrap Daily Roster 20170801master.xlsx')
 sheet = wb.get_sheet_by_name("Residential")
 otherThing = sheet.max_row
+
 
 glc_emptyList = []
 glc_asaList = []
@@ -26,17 +32,20 @@ glc_completedList = []
 glc_arrestedList = []
 glc_dischargedList = []
 
+
 do_emptyList = []
 do_asaList = []
 do_completedList = []
 do_arrestedList = []
 do_dischargedList = []
 
+
 dr_emptyList = []
 dr_asaList = []
 dr_completedList = []
 dr_arrestedList = []
 dr_dischargedList = []
+
 
 de_emptyList = []
 de_asaList = []
@@ -67,18 +76,18 @@ def glcNumbers():
     for i in glc_emptyList:
         if i == "ASA":
             glc_asaList.append(i)
-    print("Graduated percentage for GLC:" + str(len(glc_completedList)/len(glc_emptyList)))
+    print(" ")
+    print("ASA percentage for GLC:" + str(len(glc_asaList)/len(glc_emptyList)* 100) + str("%"))
 
     for i in glc_emptyList:
         if i == "Discharged":
             glc_dischargedList.append(i)
-    print("Discharged percentage for GLC:" + str(len(glc_dischargedList)/len(glc_emptyList)))
+    print("Discharged percentage for GLC:" + str(len(glc_dischargedList)/len(glc_emptyList) * 100) + str("%"))
 
     for i in glc_emptyList:
         if i == "Completed":
             glc_completedList.append(i)
-    print("Graduated percentage for GLC:" + str(len(glc_completedList)/len(glc_emptyList)))
-
+    print("Graduated percentage for GLC:" + str(len(glc_completedList)/len(glc_emptyList) * 100) + str("%"))
 
 def doughertyNumbers():
     for columnOfCellObjects in do_sheet["I93" : "I98"]:
@@ -92,12 +101,13 @@ def doughertyNumbers():
     for i in do_emptyList:
         if i == "GRAD":
             do_completedList.append(i)
-    print("Graduated percentage for IOP:" + str(len(do_completedList)/len(do_emptyList)))
+    print(" ")
+    print("Graduated percentage for IOP:" + str(len(do_completedList)/len(do_emptyList) * 100) + str("%"))
 
     for i in do_emptyList:
         if i == "Unsucc":
             do_dischargedList.append(i)
-    print("Unsuccessful percentage for IOP:" + str(len(do_dischargedList)/len(do_emptyList)))
+    print("Unsuccessful percentage for IOP:" + str(len(do_dischargedList)/len(do_emptyList) * 100) + str("%"))
 
 def droegeNumbers():
     for columnOfCellObjects in dr_sheet["I58" : "I83"]:
@@ -114,22 +124,23 @@ def droegeNumbers():
     for i in dr_emptyList:
         if i == "ASA":
             dr_asaList.append(i)
-    print("ASA percentage for Droege:" + str(len(dr_asaList)/len(dr_emptyList)))
+    print(" ")
+    print("ASA percentage for Droege:" + str(len(dr_asaList)/len(dr_emptyList) * 100) + str("%"))
 
     for i in dr_emptyList:
         if i == "Graduate":
             dr_completedList.append(i)
-    print("Graduated percentage for Droege:" + str(len(dr_completedList)/len(dr_emptyList)))
+    print("Graduated percentage for Droege:" + str(len(dr_completedList)/len(dr_emptyList) * 100) + str("%"))
 
     for i in dr_emptyList:
         if i == "Complete":
             dr_arrestedList.append(i)
-    print("Completion percentage for Droege:" + str(len(dr_arrestedList)/len(dr_emptyList)))
+    print("Completion percentage for Droege:" + str(len(dr_arrestedList)/len(dr_emptyList) * 100) + str("%"))
 
     for i in dr_emptyList:
         if i == "Discharged":
             dr_dischargedList.append(i)
-    print("Discharged percentage for Droege:" + str(len(dr_dischargedList)/len(dr_emptyList)))
+    print("Discharged percentage for Droege:" + str(len(dr_dischargedList)/len(dr_emptyList) * 100) + str("%"))
 
 def detoxNumbers():
     for columnOfCellObjects in de_sheet["I25": "I113"]:
@@ -146,22 +157,23 @@ def detoxNumbers():
     for i in de_emptyList:
         if i == "ASA":
             de_asaList.append(i)
-    print("ASA percentage for Detox:" + str(len(de_asaList) / len(de_emptyList)))
+    print(" ")
+    print("ASA percentage for Detox:" + str(len(de_asaList) / len(de_emptyList) * 100) + str("%"))
 
     for i in de_emptyList:
         if i == "Med Discharge":
             de_completedList.append(i)
-    print("MEd Discharge percentage for Detox:" + str(len(de_completedList) / len(de_emptyList)))
+    print("MEd Discharge percentage for Detox:" + str(len(de_completedList) / len(de_emptyList) * 100) + str("%"))
 
     for i in de_emptyList:
         if i == "comp":
             de_arrestedList.append(i)
-    print("Completion percentage for Detox:" + str(len(de_arrestedList) / len(de_emptyList)))
+    print("Completion percentage for Detox:" + str(len(de_arrestedList) / len(de_emptyList) * 100) + str("%"))
 
     for i in de_emptyList:
         if i == "Discharged":
             de_dischargedList.append(i)
-    print("Discharged percentage for Detox:" + str(len(de_dischargedList) / len(de_emptyList)))
+    print("Discharged percentage for Detox:" + str(len(de_dischargedList) / len(de_emptyList) * 100) + str("%"))
 
 def wrapNumbers():
     for columnOfCellObjects in sheet["H55": "H84"]:
@@ -178,26 +190,27 @@ def wrapNumbers():
     for i in emptyList:
         if i == "ASA":
             asaList.append(i)
-    print("ASA percentage for WRAP:" + str(len(asaList)/len(emptyList)))
+    print(" ")
+    print("ASA percentage for WRAP:" + str(len(asaList)/len(emptyList) * 100) + str("%"))
 
     for i in emptyList:
         if i == "Completed":
             completedList.append(i)
-    print("Completed percentage for WRAP:" + str(len(completedList)/len(emptyList)))
+    print("Completed percentage for WRAP:" + str(len(completedList)/len(emptyList) * 100) + str("%"))
 
     for i in emptyList:
         if i == "Arrested":
             arrestedList.append(i)
-    print("Arrested percentage for WRAP:" + str(len(arrestedList)/len(emptyList)))
+    print("Arrested percentage for WRAP:" + str(len(arrestedList)/len(emptyList) * 100) + str("%"))
 
     for i in emptyList:
         if i == "Discharged":
             dischargedList.append(i)
-    print("Discharged percentage for WRAP:" + str(len(dischargedList)/len(emptyList)))
+    print("Discharged percentage for WRAP:" + str(len(dischargedList)/len(emptyList) * 100) + str("%"))
 
     for i in emptyList:
         if i == "Un Succ Dis":
             dischargedList.append(i)
-    print("Un Successful Discharge percentage for WRAP:" + str(len(dischargedList)/len(emptyList)))
+    print("Un Successful Discharge percentage for WRAP:" + str(len(dischargedList)/len(emptyList) * 100) + str("%"))
 
 
