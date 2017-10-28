01f295170394156586e7495f19ad4332d9a0dc56
SethEggerRocks <setheggerrocks@gmail.com>
2017-10-27 23:36:09 -0400

    Update GUIexcelTranPer.py
    I made it to where once the button is clicked, the functions print to the terminal for now but will eventually print to a file or email or whatever, BUT the button clears with the app.destroy() and then exit() command, but I will keep the exit() command for the very last function that gets called. app.destroy will simply clear and now I am curious if I will be printing to that same widget or if it will clear once a new widget is called and I hope I can simply copy the widget and place it in a different file in the same directory and not have it conflict if it has a different name in that other file.. I am still learning .

--- GUIexcelTranPer.py
+++ GUIexcelTranPer.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import datetime
 from excelPlayin import *
-
+import time
 
 now = datetime.datetime.now()
 today = (str(now.month) + "/" + str(now.day) + "/" + str(now.year))
@@ -21,9 +21,9 @@ class Application(tk.Frame):
         self.hi_there["text"] = "\n Press Here To Get Stats!\n"
         self.hi_there["command"] = self.say_hi
         self.hi_there.pack(side="top")
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
-        self.quit.pack(side="bottom")   # This closes window when pressed
+        #self.quit = tk.Button(self, text="QUIT", fg="red",
+        #                      command=root.destroy)
+        #self.quit.pack(side="bottom")   # This closes window when pressed
 
     def say_hi(self):
         glcNumbers()
@@ -32,6 +32,10 @@ class Application(tk.Frame):
         wrapNumbers()
         doughertyNumbers()
         print(today)
+        time.sleep(3)
+        app.destroy()
+        exit()
+
 
 
 root = tk.Tk()
