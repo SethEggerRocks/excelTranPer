b93052487ee5576d1924f16f0c5361aa57b5ae01
SethEggerRocks <setheggerrocks@gmail.com>
2017-10-27 23:21:08 -0400

    Create GUIexcelTranPer.py
    This brings a GUI into the picture and I am going to integrate several and I think start experimenting with keeping all my functions for this program in one file.

---
+++ GUIexcelTranPer.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+import datetime
+from excelPlayin import *
+
+
+now = datetime.datetime.now()
+today = (str(now.month) + "/" + str(now.day) + "/" + str(now.year))
+
+
+class Application(tk.Frame):
+
+
+# This is the widget!!
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "\n Press Here To Get Stats!\n"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=root.destroy)
+        self.quit.pack(side="bottom")   # This closes window when pressed
+
+    def say_hi(self):
+        glcNumbers()
+        droegeNumbers()
+        detoxNumbers()
+        wrapNumbers()
+        doughertyNumbers()
+        print(today)
+
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
